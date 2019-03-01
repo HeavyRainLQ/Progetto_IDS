322fcee8f9128ad4031606c9998a1969dacd3bf3
Luca Quercetti <luca.quercetti@hotmail.it>
2019-03-01 17:35:28 +0100

    -c

--- progetto-IDS/src/app/log-in/log-in.component.ts
+++ progetto-IDS/src/app/log-in/log-in.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  loginForm: FormGroup;
+  @Input() username: string;
+  @Input() password: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+  });
   }
+  
+  logIn() {
+  console.log('va');
+}
 
 }
