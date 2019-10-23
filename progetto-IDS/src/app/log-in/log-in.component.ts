61df83d4369245ec18fa6b42524951b59e19a850
Luca Quercetti <luca.quercetti@hotmail.it>
2019-10-23 12:01:31 +0200

    passaggio parametro tipo user su menucomponent

--- progetto-IDS/src/app/log-in/log-in.component.ts
+++ progetto-IDS/src/app/log-in/log-in.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Injectable, OnDestroy, Output } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {Router} from '@angular/router';
-import {SqlServiceService } from '../sql-service.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SqlServiceService } from '../sql-service.service';
 import * as jQuery from 'jquery';
 
 @Component({
@@ -12,103 +12,95 @@ import * as jQuery from 'jquery';
 @Injectable({
   providedIn: 'root'
 })
-export class LogInComponent implements OnInit, OnDestroy 
-{
+export class LogInComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   @Input() username: string;
   @Input() password: string;
   disabilitato: boolean;
-  
-  message="Loading.... JOSE";
-  risposta=" ";
+
+  message = "Loading.... JOSE";
+  risposta = " ";
   //public servicio:SqlServiceService;
- constructor(private formBuilder: FormBuilder, private router: Router,private user:SqlServiceService )  
- {
-    
- }
+  constructor(private formBuilder: FormBuilder, private router: Router, private user: SqlServiceService) {
+
+  }
 
   ngOnInit() {
 
-    
 
 
-  /*$(document).ready(() => {
-    $('.loader-page').css({'visibility': 'hidden', 'opacity': '0'});
-  });*/
+
+    /*$(document).ready(() => {
+      $('.loader-page').css({'visibility': 'hidden', 'opacity': '0'});
+    });*/
 
 
-// inicia valores con campos vacios
-    this.disabilitato=true;
+    // inicia valores con campos vacios
+    this.disabilitato = true;
     this.loginForm = this.formBuilder.group({
       username: [''],
       password: ['']
-  });
+    });
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
 
   }
-  user1:string;
-  pass1:string;
-  result:Object;
-  logIn() 
-  {
-  this.user.getSomeData(this.user1,this.pass1).subscribe(data =>{ 
-    console.log(data);
+  user1: string;
+  pass1: string;
+  result: Object;
+  logIn() {
+    this.user.getSomeData(this.user1, this.pass1).subscribe(data => {
+      console.log(data);
 
       this.result = data["records"];
       //console.log($scope.result[0].ini);
-      var a=this.result[0].ini;
-      if (a=="true") 
-      {
-       
-       confirm(" Benvenuto !!");
-       if (this.result[0].estado=="1") 
-       {    
-         if (this.result[0].tipo=="1") 
-           {
-               //$rootScope.user = true;
-               //console.log($rootScope.user);
-               //location.href='#/main';       
-               this.router.navigate(['/area-riservata']);
-           }
-           else
-           {              
-             //$rootScope.user = false;
-             //location.href='#/main';
-             this.router.navigate(['/area-riservata']);
-           }
-       }
-       else
-       {
-           alert("Usted se encuentra deshabilitado");
-       }
+      var a = this.result[0].ini;
+      if (a == "true") {
 
-      } 
-      else{
-        this.user1="";
-        this.pass1="";
-        this.risposta="Sorry, error user or password!";
+        confirm(" Benvenuto !!");
+        if (this.result[0].estado == "1") {
+          if (this.result[0].tipo == "1") {
+            //$rootScope.user = true;
+            //console.log($rootScope.user);
+            //location.href='#/main';       
+            this.router.navigate(['/area-riservata']);
+          }
+          else {
+            //$rootScope.user = false;
+            //location.href='#/main';
+            this.router.navigate(['/area-riservata']);
+          }
+        }
+        else {
+          alert("Usted se encuentra deshabilitado");
+        }
+
       }
-  });
+      else {
+        this.user1 = "";
+        this.pass1 = "";
+        this.risposta = "Sorry, error user or password!";
+      }
+    });
 
-  console.log(this.loginForm.controls.username.value);
-  console.log(this.loginForm.controls.password.value);
+    console.log(this.loginForm.controls.username.value);
+    console.log(this.loginForm.controls.password.value);
 
-}
-//--------------------------------
-
-
+  }
+  //--------------------------------
 
 
-//---------------------------------
 
-login_user(event){
-event.preventDefault()
-const target = event.target
-const username = target.querySelector('#username')
-const pass = target.querySelector('#pasword')
 
-console.log(username,pass)
-}
+  //---------------------------------
+
+  login_user(event) {
+    event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username')
+    const pass = target.querySelector('#pasword')
+
+    console.log(username, pass)
+  }
 
 }
--- progetto-IDS/src/app/sql-service.service.ts
+++ progetto-IDS/src/app/sql-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 interface myData {
   message: string,
@@ -8,152 +8,152 @@ interface myData {
 }
 
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
 
 
 export class SqlServiceService {
 
   valori: Array<string>;
-  var1:object;
+  var1: object;
+  utente: Object;
+  a;
 
 
-  constructor(private http:HttpClient) 
-  {
+  constructor(private http: HttpClient) {
   }
   //Generamos las funciones que nos serviran para manipular nuestras entidades
-  getSomeData(user, pass)
-  {
+  getSomeData(user, pass) {
     console.log("llego al servicio getSomeData: ->");
+
     //return this.http.get<myData>(`${this.URL}conexion.php`);
     //return this.http.get("/api/conexion.php");
-    return this.http.post('/api/login.php', {
+    this.a = this.http.post('/api/login.php', {
       user,
       pass
-    })
+    });
+    this.a.subscribe(data => {
+      this.utente = data["records"];
+    });
+    return this.a;
+    // return this.http.post('/api/login.php', {
+    //   user,
+    //   pass
+    // })
     //return this.http.get<myData>("../../../src/app/conexion.php");
-  
+
   }//fin funcion de login
 
-  mio_profilo(user, pass)
-  {
+  mio_profilo(user, pass) {
     console.log("servicio mio profilo: ->");
     return this.http.post('/api/mio_profilo.php', {
       user,
       pass
     })
-    
+
   }//fin del metodo mio_profilo
 
-  insert_misura(tariffa,conta,lavoro,perce,ris)
-  {
+  insert_misura(tariffa, conta, lavoro, perce, ris) {
 
 
     console.log("servicio insert misura: ->");
     //return this.http.get<myData>(`${this.URL}conexion.php`);
     //return this.http.get("/api/conexion.php");
     return this.http.post('/api/insert_misura.php', {
-      tariffa,conta,lavoro,perce,ris
+      tariffa, conta, lavoro, perce, ris
     })
-    
+
   }//fin del metodo mio_profilo
 
-  select_descrizione(nomeCat,descrizione)
-  {
+  select_descrizione(nomeCat, descrizione) {
 
     console.log("servicio select_descrizione: ->");
     return this.http.post('/api/select_categoria.php', {
-      nomeCat,descrizione
+      nomeCat, descrizione
     })
-    
+
   }//fine select descrizione
 
-  select_attrezzatura()
-  {
-    return this.http.post('/api/select_attre.php',{})
-    
+  select_attrezzatura() {
+    return this.http.post('/api/select_attre.php', {})
+
   }//fine select attrezzatura
 
-  select_qualifica()
-  {
-        
-    return this.http.post('/api/select_qualifica.php',{})
-    
+  select_qualifica() {
+
+    return this.http.post('/api/select_qualifica.php', {})
+
   }//fine select attrezzatura
-  contabilita(budget)
-  {
+  contabilita(budget) {
     console.log("servicio contabilita: ->");
     return this.http.post('/api/select_contabilita.php', {
       budget
     });
-    
+
   }//fine contabilita
 
-  
 
-  updateApprova(item_id)
-  {
-     console.log("servicio update approva: ->");
-     return this.http.post('/api/update_approva.php', {
+
+  updateApprova(item_id) {
+    console.log("servicio update approva: ->");
+    return this.http.post('/api/update_approva.php', {
       item_id
-     })
+    })
   }//fine update invalida
 
-  updateInvalida(item_id)
-  {
-     console.log("servicio contabilita: ->");
-     return this.http.post('/api/update_valida.php', {
+  updateInvalida(item_id) {
+    console.log("servicio contabilita: ->");
+    return this.http.post('/api/update_valida.php', {
       item_id
-     })
+    })
   }//fine update invalida
 
-  update_info(dati:object)
-  {
+  update_info(dati: object) {
     console.log("servicio update: ->");
     console.log(dati);
-    this.var1=dati["col1"];
+    this.var1 = dati["col1"];
     console.log(this.var1["ci"]);
-    
+
 
     /*
 */
 
 
     //return this.http.get("/api/conexion.php");
-    return this.http.post('/api/update_user.php',         
-      
+    return this.http.post('/api/update_user.php',
+
       this.var1
     )
     //return this.http.get<myData>("../../../src/app/conexion.php");
   }//fin del metodo update
 
-logueo(user: string,pass:string) {
-    
+  logueo(user: string, pass: string) {
+
     console.log("llego al servicio 2::");
-     //this.http.get('http://localhost/myCRUD/src/api/productos.php?user='+user+'&pass='+pass+'');
+    //this.http.get('http://localhost/myCRUD/src/api/productos.php?user='+user+'&pass='+pass+'');
     console.log(this.http.post('http://localhost/ids3/Progetto_ids/progetto-ids/src/app/mis_php/login.php',
-     {
-      "user": user,
-      "pass":pass
-     }
-     ));
+      {
+        "user": user,
+        "pass": pass
+      }
+    ));
 
 
-     //this.http.post("http://localhost/Hall-Management-System/api/login.php",{"index_signin":index_signin,"password_signin":password_signin}).map(res=>res.json())
-     
-     console.log("datos del usuario");
-     //console.log(this.datos);
+    //this.http.post("http://localhost/Hall-Management-System/api/login.php",{"index_signin":index_signin,"password_signin":password_signin}).map(res=>res.json())
 
-  //   $scope.result = response.records;
-  //console.log($scope.result[0].ini);
-  //  var a=$scope.result[0].ini; 
+    console.log("datos del usuario");
+    //console.log(this.datos);
+
+    //   $scope.result = response.records;
+    //console.log($scope.result[0].ini);
+    //  var a=$scope.result[0].ini; 
   }
   detallar(id: number) {
     return this.http.get('http://localhost/myCRUD/src/api/productos.php?opcion=2&id=' + id);
   }
   guardar(item: Object) {
     return this.http.post('http://localhost/myCRUD/src/api/productos.php?opcion=3', item);
-  } 
+  }
   modificar(item: Object) {
     return this.http.post('http://localhost/myCRUD/src/api/productos.php?opcion=4', item);
   }
